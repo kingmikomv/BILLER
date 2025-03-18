da010d33860ca32d0f149c706d204fbc9d7b2f5d
Lun4t1c <lun4t1c@localhost.localdomain>
2025-03-18 21:10:26 +0700

    Update SUMIN

--- resources/views/ROLE/SUMIN/index.blade.php
+++ resources/views/ROLE/SUMIN/index.blade.php
@@ -1,1 +1,65 @@
-INI SUMIN
+<x-dhs.head />
+
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+
+        <x-dhs.preload />
+
+        <!-- Navbar -->
+        <x-dhs.nav />
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <x-dhs.sidebar />
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper" style="margin-bottom: 50px">
+            <!-- Content Header (Page header) -->
+            <x-dhs.content-header />
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <!-- Info boxes -->
+                 
+                    <div class="row">
+
+                        <div class="col-md-12">
+
+                            <div class="card">
+                                <div class="card-header border-transparent">
+                                    <h3 class="card-title">Riwayat Pemasangan Baru</h3>
+                                </div>
+                                <!-- /.card-header -->
+                                <div class="card-body table-responsive">
+                                    
+                                </div>
+                                <!-- /.card-body -->
+
+                                <!-- /.card-footer -->
+                            </div>
+
+
+                        </div>
+
+
+                    </div>
+
+
+                </div>
+
+            </section>
+            <!-- /.content -->
+        </div>
+        
+
+        <x-dhs.footer />
+    </div>
+    <!-- ./wrapper -->
+
+    <x-dhs.scripts />
+    
+</body>
+
+</html>
