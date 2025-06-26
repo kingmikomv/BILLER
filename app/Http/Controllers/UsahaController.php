0a85c47fff2f8b1b900bb711029c112a565b7278
kingmikomv <kingmikomv@gmail.com>
2025-06-26 17:13:17 +0700

    Update Usaha

--- app/Http/Controllers/UsahaController.php
+++ app/Http/Controllers/UsahaController.php
@@ -84,7 +84,7 @@ class UsahaController extends Controller
             }
     
             // Buat nama file unik dan simpan sebagai JPEG
-            $filename = 'Logo.jpg';
+            $filename = 'Logo.jpeg';
             $fullPath = $uploadPath . $filename;
     
             if (!imagejpeg($src, $fullPath, 85)) { // 85 = kualitas jpeg (0-100)
